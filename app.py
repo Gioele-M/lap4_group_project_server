fff63c3cdbc1c175150635df200e576cbf76c83c
Gioele-M <gioelemontis97@gmail.com>
2022-07-14 19:06:27 +0100

    connect to mongo locally works, trying deploying

--- app.py
+++ app.py
@@ -1,15 +1,34 @@
-from re import A
 from flask import Flask, request
 from flask_cors import CORS
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
-CORS(app)
 
+load_dotenv()
+#{os.environ.get('password')}
+
+connection_url = f"mongodb+srv://gio:gio@cluster0.er7j3.mongodb.net/?retryWrites=true&w=majority"
+
+client = MongoClient(connection_url)
+
+db = client.get_database('wewa')
+# the two collections are playlist and users
+
+
+CORS(app)
 
 @app.route('/', methods=['GET'])
 def home_route():
-    return 'This is the home page', 200
+    users = db.users.find()
+    return str(users), 200
 
 
+
+@app.route('/newuser')
+def new_user():
+    db.users.insert_one({'username': 'testuser'})
+    return 'True', 204
 
 
