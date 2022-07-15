1805e86d178064b257cd9eb68d192118de76074c
Gioele-M <gioelemontis97@gmail.com>
2022-07-15 11:40:13 +0100

    sample endpoints for frontend developmet

--- app.py
+++ app.py
@@ -20,16 +20,93 @@ db = client.get_database('wewa')
 
 CORS(app)
 
-@app.route('/', methods=['GET'])
-def home_route():
-    users = db.users.find()
-    return str(users), 200
+users_model = {
+    'username': 'Matteo',
+    'hashedPassword': 'notsohashed',
+    'userEmail': 'matteo@gmail.com',
+    'token': 'token_string',
+    'previousTokens': ['token1', 'token2'],
+    'lastSelection': {'selectedNote': 'chapter1', 'selectedPlaylist': 'playlistName'},
+    'recentNotes': [
+        {'selectedNote': 'chapter2', 'selectedPlaylist': 'playlistNameX'}, 
+        {'selectedNote': 'chapter3', 'selectedPlaylist': 'playlistNameY'}
+        ],
+    'favourites': ['playlistNameX', 'playlistNameY'],
+    'achievement': ['badgeX', 'badgeY']
+}
+
+playlist_model = {
+    'playlistName': 'playlistX',
+    'playlistOwner': 'matteo@gmail.com',
+    'editingAccess': ['gioele@gmail.com', 'igor@gmail.com'],
+    'public': True,
+    'playlistTheme': '(0,0,0)',
+    'averageStars': {
+        'currentRating': 3.5,
+        'totalStars': 7,
+        'totalRatings': 2
+    },
+    'userStars': [
+        {'userEmail': 'gioele@gmail.com', 'rating': 4},
+        {'userEmail': 'igor@gmail.com', 'rating': 3}
+    ],
+    'commentSection': [
+        {
+            'commentID': 1, #sequential
+            'username': 'Matteo',
+            'userEmail': 'matteo@gmail.com',
+            'comment': 'This playlist is great :)',
+            'thread': [
+                {
+                    'username': 'Gio',
+                    'userEmail': 'gioele@gmail.com',
+                    'reply': 'great indeed!'
+                },
+                {
+                    'username': 'Igor',
+                    'userEmail': 'igor@gmail.com',
+                    'reply': 'Looks great but I would add another section!'
+                }
+            ]
+
+        }
+    ],
+    'chapters': [
+        {
+            'chapter1': {
+                'chapterTitle': 'first chapter',
+                'video_url': 'youtube.com',
+                'start': '0.00',
+                'end': '0.30',
+                'text': 'This is the section with the notes on the first chapter'
+            }
+
+            
+        }
+    ]
+}
 
 
 
-@app.route('/newuser')
-def new_user():
-    db.users.insert_one({'username': 'testusxxxer'})
-    return 'True', 204
 
 
+@app.route('/userSample', methods=['GET'])
+def user_sample():
+    return users_model, 200
+
+
+@app.route('/playlistSample')
+def playlist_sample():
+    return playlist_model, 200
+
+
+
+# @app.route('/newuser')
+# def new_user():
+#     db.users.insert_one({'username': 'testusxxxer'})
+#     return 'True', 204
+
+
+# def find_all():
+#     users = db.users.find()
+#     return str(users), 200
