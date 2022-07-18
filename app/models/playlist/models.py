e484f0576f78798c6b2b8168b7023499b63844b0
Gioele-M <gioelemontis97@gmail.com>
2022-07-18 11:41:34 +0100

    handle editing privileges

--- app/models/playlist/models.py
+++ app/models/playlist/models.py
@@ -89,44 +89,71 @@ class Playlist:
     def patch(self):
         data = request.get_json()
         playlistName = data['playlistName']
+        userRequesting = data['userRequesting']
         _patch_term = list(data.keys())
         _patch_term.remove('playlistName')
+        _patch_term.remove('userRequesting')
         patch_term = _patch_term[0]
         
-        # Patch accessibility
-        if patch_term == 'public':
-            if data[patch_term] == 'True' or data[patch_term] == 'False':
-                playlist = db.playlists.find_one_and_update(
-                    {'playlistName': playlistName},
-                    {'$set': {patch_term: data[patch_term]}},
-                    return_document = ReturnDocument.AFTER
-                    )
+        #Check if playlist to interact exists
+        to_patch = db.playlists.find_one({'playlistName': playlistName})
+        if to_patch:
+            
+            # Check if who sent has editing access
+            if to_patch['playlistOwner'] == userRequesting or userRequesting in to_patch['editingAccess']:
+
+
+                # Patch accessibility
+                if patch_term == 'public':
+                    if data[patch_term] == 'True' or data[patch_term] == 'False':
+                        playlist = db.playlists.find_one_and_update(
+                            {'playlistName': playlistName},
+                            {'$set': {patch_term: data[patch_term]}},
+                            return_document = ReturnDocument.AFTER
+                            )
+                    else:
+                        return jsonify({'error': 'You have to set public as either True or False (string)'})
+
+                # Patch playlist name
+                if patch_term == 'newName':
+                    if db.playlists.find_one({'playlistName': data[patch_term]}):
+                        return jsonify({'error': 'Playlist name already in use'}), 406
+                    
+                    playlist = db.playlists.find_one_and_update(
+                            {'playlistName': playlistName},
+                            {'$set': {'playlistName': data[patch_term]}},
+                            return_document = ReturnDocument.AFTER
+                            )
+                    #find if the name is already used
+                    
+                
+                # Patch theme
+                if patch_term == 'playlistTheme':
+                    playlist = db.playlists.find_one_and_update(
+                            {'playlistName': playlistName},
+                            {'$set': {patch_term: data[patch_term]}},
+                            return_document = ReturnDocument.AFTER
+                            )
+
+
+                # Patch tags 
+                if patch_term == 'tags':
+                    
+                    pass
+
+
+                if patch_term == 'editingAccess':
+                    pass
+
+
+                if patch_term == 'chapters':
+                    pass
+            
             else:
-                return jsonify({'error': 'You have to set public as either True or False (string)'})
+                return jsonify({'error': 'You do not have editing access for this playlist'})
 
-        # Patch playlist name
-        if patch_term == 'newName':
-            if db.playlists.find_one({'playlistName': data[patch_term]}):
-                return jsonify({'error': 'Playlist name already in use'}), 406
-            
-            playlist = db.playlists.find_one_and_update(
-                    {'playlistName': playlistName},
-                    {'$set': {'playlistName': data[patch_term]}},
-                    return_document = ReturnDocument.AFTER
-                    )
-            #find if the name is already used
-            
-        
-        # Patch theme
-        if patch_term == 'playlistTheme':
-            pass
-
-
-        # Patch tags 
-        if patch_term == 'tags':
-            pass
-
-
+        else:
+            return jsonify({'error': 'No playlist with that name'})
 
 
         print(playlist, flush=True)
